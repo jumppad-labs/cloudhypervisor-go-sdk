6905d209d7ba7151a22308be70d1ec2ce13c3568
Erik Veld <erik.veld@gmail.com>
2024-06-13 10:30:22 +0200

    Use other lib to generate cloudinit

--- disk.go
+++ disk.go
@@ -1,15 +1,11 @@
 package sdk
 
 import (
-	"fmt"
-	"io"
 	"os"
+	"path"
 	"path/filepath"
 
-	diskfs "github.com/diskfs/go-diskfs"
-	"github.com/diskfs/go-diskfs/disk"
-	"github.com/diskfs/go-diskfs/filesystem"
-	"github.com/diskfs/go-diskfs/filesystem/iso9660"
+	"github.com/kdomanski/iso9660"
 )
 
 func folderSize(path string) (int64, error) {
@@ -21,7 +17,6 @@ func folderSize(path string) (int64, error) {
 		if !info.IsDir() {
 			size += info.Size()
 		}
-
 		return err
 	})
 
@@ -35,81 +30,175 @@ func folderSize(path string) (int64, error) {
 	return size, err
 }
 
+// func createISO9660Disk(source string, label string, destination string) error {
+// 	folderSize, err := folderSize(source)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	var LogicalBlocksize diskfs.SectorSize = 2048
+// 	mydisk, err := diskfs.Create(destination, folderSize, diskfs.Raw, LogicalBlocksize)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	fspec := disk.FilesystemSpec{
+// 		Partition:   0,
+// 		FSType:      filesystem.TypeISO9660,
+// 		VolumeLabel: label,
+// 	}
+// 	fs, err := mydisk.CreateFilesystem(fspec)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+// 		if err != nil {
+// 			return err
+// 		}
+
+// 		relPath, err := filepath.Rel(source, path)
+// 		if err != nil {
+// 			return err
+// 		}
+
+// 		if info.IsDir() {
+// 			err = fs.Mkdir(relPath)
+// 			if err != nil {
+// 				return err
+// 			}
+// 			return nil
+// 		}
+
+// 		if !info.IsDir() {
+// 			rw, err := fs.OpenFile(relPath, os.O_CREATE|os.O_RDWR)
+// 			if err != nil {
+// 				return err
+// 			}
+
+// 			in, errorOpeningFile := os.Open(path)
+// 			if errorOpeningFile != nil {
+// 				return errorOpeningFile
+// 			}
+// 			defer in.Close()
+
+// 			_, err = io.Copy(rw, in)
+// 			if err != nil {
+// 				return err
+// 			}
+// 		}
+
+// 		return nil
+// 	})
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	iso, ok := fs.(*iso9660.FileSystem)
+// 	if !ok {
+// 		if err != nil {
+// 			return fmt.Errorf("not an iso9660 filesystem")
+// 		}
+// 	}
+// 	err = iso.Finalize(iso9660.FinalizeOptions{})
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
 func createISO9660Disk(source string, label string, destination string) error {
-	folderSize, err := folderSize(source)
+	writer, err := iso9660.NewWriter()
+	if err != nil {
+		return err
+	}
+	defer writer.Cleanup()
+
+	output, err := os.MkdirTemp("", "cloudinit-*")
 	if err != nil {
 		return err
 	}
 
-	var LogicalBlocksize diskfs.SectorSize = 2048
-	mydisk, err := diskfs.Create(destination, folderSize, diskfs.Raw, LogicalBlocksize)
+	metadataSource := path.Join(source, "meta-data")
+	metadata, err := os.ReadFile(metadataSource)
 	if err != nil {
 		return err
 	}
 
-	fspec := disk.FilesystemSpec{
-		Partition:   0,
-		FSType:      filesystem.TypeISO9660,
-		VolumeLabel: label,
-	}
-	fs, err := mydisk.CreateFilesystem(fspec)
+	metadataDestination := filepath.Join(output, "meta-data")
+	err = os.WriteFile(metadataDestination, []byte(metadata), 0644)
 	if err != nil {
 		return err
 	}
 
-	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
+	mf, err := os.Open(metadataDestination)
+	if err != nil {
+		return err
+	}
+	defer mf.Close()
 
-		relPath, err := filepath.Rel(source, path)
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			err = fs.Mkdir(relPath)
-			if err != nil {
-				return err
-			}
-
-			return nil
-		}
-
-		if !info.IsDir() {
-			rw, err := fs.OpenFile(relPath, os.O_CREATE|os.O_RDWR)
-			if err != nil {
-				return err
-			}
-
-			in, errorOpeningFile := os.Open(path)
-			if errorOpeningFile != nil {
-				return errorOpeningFile
-			}
-			defer in.Close()
-
-			_, err = io.Copy(rw, in)
-			if err != nil {
-				return err
-			}
-		}
-
-		return nil
-	})
+	err = writer.AddFile(mf, "meta-data")
 	if err != nil {
 		return err
 	}
 
-	iso, ok := fs.(*iso9660.FileSystem)
-	if !ok {
-		return fmt.Errorf("not an iso9660 filesystem")
-	}
-
-	err = iso.Finalize(iso9660.FinalizeOptions{})
+	userdataSource := path.Join(source, "user-data")
+	userdata, err := os.ReadFile(userdataSource)
 	if err != nil {
 		return err
 	}
 
+	userdataDestination := path.Join(output, "user-data")
+	err = os.WriteFile(userdataDestination, []byte(userdata), 0644)
+	if err != nil {
+		return err
+	}
+
+	uf, err := os.Open(userdataDestination)
+	if err != nil {
+		return err
+	}
+	defer uf.Close()
+
+	err = writer.AddFile(uf, "user-data")
+	if err != nil {
+		return err
+	}
+
+	networkConfigSource := path.Join(source, "user-data")
+	networkConfig, err := os.ReadFile(networkConfigSource)
+	if err != nil {
+		return err
+	}
+
+	networkConfigDestination := path.Join(output, "network-config")
+	err = os.WriteFile(networkConfigDestination, []byte(networkConfig), 0644)
+	if err != nil {
+		return err
+	}
+
+	nf, err := os.Open(networkConfigDestination)
+	if err != nil {
+		return err
+	}
+	defer nf.Close()
+
+	err = writer.AddFile(nf, "network-config")
+	if err != nil {
+		return err
+	}
+
+	of, err := os.OpenFile(destination, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer of.Close()
+
+	err = writer.WriteTo(of, "cidata")
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -4,27 +4,19 @@ go 1.21.5
 
 require (
 	github.com/charmbracelet/log v0.4.0
-	github.com/diskfs/go-diskfs v1.4.0
 	github.com/kdomanski/iso9660 v0.4.0
 )
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.10.0 // indirect
-	github.com/elliotwutingfeng/asciiset v0.0.0-20230602022725-51bbb787efab // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
-	github.com/pierrec/lz4/v4 v4.1.17 // indirect
-	github.com/pkg/xattr v0.4.9 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/sirupsen/logrus v1.9.0 // indirect
-	github.com/ulikunitz/xz v0.5.11 // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/sys v0.20.0 // indirect
-	gopkg.in/djherbis/times.v1 v1.3.0 // indirect
 )
